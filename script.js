12fb9c31a2df623c3c19eab8f984cffd2b6e2dab
ArdaniPaemba <114474482+ArdaniPaemba@users.noreply.github.com>
2025-06-09 10:03:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burger.classList.toggle('toggle');
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+window.addEventListener('load', () => {
+    document.querySelectorAll('.animate-fade, .animate-slide').forEach((element) => {
+        element.style.animationPlayState = 'running';
+    });
+});
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
